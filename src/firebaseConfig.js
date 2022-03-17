3d71e8459a9064b8b81bfdf8f445a1c877c5232e
Muhammad Irfan <irfhan025@gmail.com>
2022-03-17 05:30:36 -0700

    add firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQIxH-R01Um9VV1-BZ38ZhpmpYkJBI-RE",
+  authDomain: "disney-hotstar-clone-16063.firebaseapp.com",
+  projectId: "disney-hotstar-clone-16063",
+  storageBucket: "disney-hotstar-clone-16063.appspot.com",
+  messagingSenderId: "470808395371",
+  appId: "1:470808395371:web:f5a0ac4fb542a865f8fb2e",
+  measurementId: "G-XM0WJNEHBW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
